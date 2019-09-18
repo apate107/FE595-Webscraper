2bd6d70c6f4d51478b335fafed1e4a9788e1d177
apate107 <apate107@stevens.edu>
2019-09-17 23:56:31 -0400

    Added full functionality

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_story(url):
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
+    data = soup.findAll("p")[1].text # The second <p> tag has the sentences
+    male = data[data.find("He's"):data.find(".")]
+    female = data[data.find("She's"):data.find(".", data.find(".")+1)]
+    return male, female
+
+
+def save_to_file(male, female):
+    with open("male.txt", "a") as f:
+        f.write(male + "\n")
+        f.close()
+    with open("female.txt", "a") as f:
+        f.write(female + "\n")
+        f.close()
+
+
+if __name__ == '__main__':
+    URL = 'https://theyfightcrime.org/'
+    male, female = None, None
+    for i in range(0, 50):
+        male, female = get_story(URL)
+        save_to_file(male, female)
